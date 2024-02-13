62edcef588f10bbf8c2683b452c6b2b2481edc67
yan <eugenescat@gmail.com>
2024-02-13 01:51:09 -0500

    commits skeleton

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,5 +1,10 @@
 package gitlet;
 
+import net.sf.saxon.functions.ConstantFunction;
+
+import java.util.Arrays;
+import java.util.List;
+
 /** Driver class for Gitlet, a subset of the Git version-control system.
  *  @author TODO
  */
@@ -10,15 +15,91 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO: what if args is empty?
+        if (args.length == 0) {
+            System.out.println("Please enter a command.");
+            return;
+        }
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
-                // TODO: handle the `init` command
+                // handle the `init` command
+                validateNumArgs("init", args, 1);
+                try {
+                    Repository repo = new Repository("init");
+                    repo.makeInitialCommit("initial commit");
+                } catch (RuntimeException e) {
+                    System.out.println(e.getMessage());
+                    return;
+                }
                 break;
             case "add":
-                // TODO: handle the `add [filename]` command
+                // handle the `add [filename]` command
+                validateNumArgs("add", args, 2);
+                try {
+                    Repository.getInstance().addFile(args[1]);
+                } catch (RuntimeException e) {
+                    System.out.println(e.getMessage());
+                    return;
+                }
                 break;
-            // TODO: FILL THE REST IN
+            case "commit":
+                // handle the `commit [message]` command
+                validateNumArgs("commit", args, 2);
+                try {
+                    Repository.getInstance().makeCommit(args[1]);
+                } catch (RuntimeException e) {
+                    System.out.println(e.getMessage());
+                    return;
+                }
+                break;
+            case "checkout":
+                // TODO: handle the `checkout [file name]` command
+
+                // TODo: handle the `checkout [commit id] -- [file name]` command
+
+                // TODO: handle the `checkout [branch name]` command
+
+            case "log":
+                // handle the `log` command
+                validateNumArgs("log", args, 1);
+                Repository.getInstance().printLog();
+
+                // ------------above is checkpoint---------------
+
+            case "rm":
+                // handle the `rm [file name]` command
+                validateNumArgs("rm", args, 2);
+                try {
+                    Repository.getInstance().removeFile(args[1]);
+                } catch (RuntimeException e) {
+                    System.out.println(e.getMessage());
+                    return;
+                }
+            case "global-log":
+                // ToDo: handle the `global-log` command
+                validateNumArgs("global-log", args, 1);
+                Repository.getInstance().printGlobalLog();
+            case "find":
+                // ToDo: handle the `find [commit message]` command
+            case "status":
+                // Todo: handle the `status` command
+            case "branch":
+                // Todo: handle the `branch [branch name]` command
+            case "rm-branch":
+                // Todo: handle the `rm-branch [branch name]` command
+            case "reset":
+                // Todo: handle the `reset [commit id]` command
+            case "merge":
+                // Todo: handle the `merge [branch name]` command
+            default :
+                System.out.println("No command with that name exists.");
+        }
+    }
+
+    public static void validateNumArgs(String cmd, String[] args, int n) {
+        if (args.length != n) {
+            throw new RuntimeException(
+                    String.format("Invalid number of arguments for: %s.", cmd));
         }
     }
 }
